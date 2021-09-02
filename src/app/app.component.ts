da0afd8748589fddd5fc06796263d79e7a21b0f0
Zsolt Barkó <Zsolt.Barko@heitec.hu>
2021-09-02 16:19:37 +0200

    feat(app): separate cases; add phases; remove unnecessary content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, QueryList, ViewChild, ViewChildren} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,26 +7,46 @@ import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angul
 })
 export class AppComponent implements AfterViewInit, OnDestroy{
   public title = 'EventBubblingTest';
-  @ViewChild('targetElement') private targetElement: ElementRef;
-  private listener: EventListenerOrEventListenerObject = (e: Event) => console.log('Other handler is running too!', e);
-  private target: HTMLElement;
+  @ViewChildren('targetElement') private targetElements: QueryList<ElementRef>;
+  @ViewChild('fakeDocument') private fakeDocument: ElementRef;
+  private fakeDoc: Element[];
+  private listener: EventListenerOrEventListenerObject = (e: Event) => {
+    const msg = 'Other handler is running too';
+    // tslint:disable-next-line:no-string-literal
+    console.log(msg, e.target['tagName']);
+    // tslint:disable-next-line:no-string-literal
+    alert(`${msg} on ${e.target['tagName']}`);
+  }
 
   public ngAfterViewInit(): void {
-    this.target = this.targetElement.nativeElement;
-    this.target.addEventListener('click', this.listener, false);
+    this.targetElements.forEach(({ nativeElement }) => nativeElement.addEventListener('click', this.listener, false));
+    this.fakeDoc = Array.from(
+      (this.fakeDocument.nativeElement as HTMLElement).getElementsByTagName('*'))
+      .concat([this.fakeDocument.nativeElement]);
+    for (const element of this.fakeDoc) {
+      element.addEventListener('click', e => alert(`Capturing: ${element.tagName}`), true);
+      element.addEventListener('click', e => alert(`Bubbling: ${element.tagName}`), false);
+    }
   }
 
   public ngOnDestroy(): void {
-    this.target.removeEventListener('click', this.listener, false);
+    this.targetElements.forEach(({ nativeElement }) => nativeElement.removeEventListener('click', this.listener, false));
+    for (const element of this.fakeDoc) {
+      element.removeEventListener('click', e => alert(`Capturing: ${element.tagName}`), true);
+      element.removeEventListener('click', e => alert(`Bubbling: ${element.tagName}`), false);
+    }
   }
 
   public bubbling(event: Event, stopBubbling: boolean = false, immediateStop: boolean = false): void {
 
-    if (stopBubbling) event.stopPropagation();
-    if (immediateStop) {
+    if (stopBubbling) {
+      event.stopPropagation();
+    } else if (immediateStop) {
       event.stopImmediatePropagation();
     }
+    // tslint:disable-next-line:no-string-literal
     console.log('It\'s bubbling!', `${event.currentTarget['tagName']} - ${event.currentTarget['className']}`, event);
+    // tslint:disable-next-line:no-string-literal
     alert(`It's bubbling through ${event.currentTarget['tagName']} and started from ${event.target['tagName']}`);
   }
 }
