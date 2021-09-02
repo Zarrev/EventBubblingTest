59061ba294696bcd8cbeba763449e004180d9fda
Barkó Zsolt <barko.zsolte@gmail.com>
2021-09-02 08:43:54 +0200

    feat(app): add observation of bubbling stop methodes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import {Component, ElementRef} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'EventBubblingTest';
+export class AppComponent implements AfterViewInit{
+  public title = 'EventBubblingTest';
+  @ViewChild('targetElement') private targetElement: ElementRef;
 
-  public bubbling(event: Event): void {
+  public ngAfterViewInit(): void {
+    console.log(this.targetElement);
+    this.targetElement.nativeElement.addEventListener('click', (e) =>
+    {
+      console.log('Other handler is running too!')
+    }, false);
+  }
+
+  public bubbling(event: Event, stopBubbling: boolean = false, immediateStop: boolean = false): void {
+
+    if (stopBubbling) event.stopPropagation();
+    if (immediateStop) {
+      event.stopImmediatePropagation();
+    }
     console.log('It\'s bubbling!', `${event.currentTarget['tagName']} - ${event.currentTarget['className']}`, event);
     alert(`It's bubbling through ${event.currentTarget['tagName']} and started from ${event.target['tagName']}`);
   }
